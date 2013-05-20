fcbb1f14dd9d4b62ea7ef3e3082272b0cceb5291
Revaz <nevillecamus@gmail.com>
2013-05-20 23:13:49 +0400

    bluring example initial commit

---
+++ js/layout.js
@@ -0,0 +1,34 @@
+closMyModal = function(){
+    $('#myModal').css({
+        'opacity' : '0'
+    });
+    $('#cover').css({
+        'visibility' : 'hidden'
+    });
+    $('#myform').fadeIn();
+}
+
+
+$(document).ready(function(){
+
+	$('#clickme').stop().click(function(){
+		var value = $(this).parent().find(".text").val();
+		$('#myModal').css({
+			'opacity' : '1',
+			'visibility' : 'visible'
+		});
+		$('#cover').css({
+			'visibility' : 'visible'
+		});
+		$('#myform').fadeOut();
+
+		$('#editable').text(value);
+	});
+	$('#exit').stop().click(closMyModal);
+
+   $(document).keyup(function(e){
+        if(e.keyCode==27){
+            closMyModal();
+        }
+   });
+});
